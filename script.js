750fe351edfcf847b9b91406eb0d67444819356c
Anton Angeletti <angelettianton@gmail.comj>
2022-01-10 20:49:19 -0600

    tasty

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function wrangle() {
-  function getSolution() {
-    const a = localStorage.gameState.split('solution')[1];
-    return a.slice(a.indexOf(':') + 2, a.indexOf(',') - 1);
-  }
+function getSolution() {
+  const a = localStorage.gameState.split('solution')[1];
+  return a.slice(a.indexOf(':') + 2, a.indexOf(',') - 1);
+}
 
-  const keyboardElement = document.querySelector('game-app').$keyboard.$keyboard.children;
-  const keyElements = {};
+function type(letters) {
+  const ary = letters.split('');
+  ary.forEach(letter => {
+    keyElements[letter].click();
+  });
+}
 
-  for (let i = 0; i < keyboardElement.length; i++) {
-    const row = keyboardElement[i].children;
-    for (let j = 0; j < row.length; j++) {
-      const key = row[j];
-      const attr = key.getAttribute('data-key'); // will be the letter of the key e.g. 'q' if it is a key, null otherwise
-      if (attr) keyElements[attr] = key;
-    }
-  }
+const keyboardElement = document.querySelector('game-app').$keyboard.$keyboard.children;
+const keyElements = {};
 
-  while (true) {
-    const inp = prompt('click which letter?');
-    keyElements[inp].click();
+for (let i = 0; i < keyboardElement.length; i++) {
+  const row = keyboardElement[i].children;
+  for (let j = 0; j < row.length; j++) {
+    const key = row[j];
+    const attr = key.getAttribute('data-key'); // will be the letter of the key e.g. 'q' if it is a key, null otherwise
+    if (attr) keyElements[attr] = key;
+    if (attr == '←') keyElements['<'] = key;
+    if (attr == '↵') keyElements['>'] = key;
   }
 }
